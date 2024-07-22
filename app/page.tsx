7f68d3188a49a4201ddfc3aa6546ff06e1938c48
Sehej Wahla <sehej@sendatradie.com>
2024-07-22 14:45:58 +1000

    successfully set up authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
+'use client';
 
+import Image from "next/image";
+import { Unauthenticated } from "convex/react";
+import { SignInButton } from "@clerk/nextjs";
+import { Authenticated } from "convex/react";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return (<>miro clone!</>)
+  return (<>     <Unauthenticated>
+    <SignInButton />
+  </Unauthenticated>
+  <Authenticated>
+    <UserButton />
+  </Authenticated></>)
+  
 }
